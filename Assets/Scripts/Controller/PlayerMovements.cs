00aed2a5443d019e4f0631ce1b973977c12b2099
Simon <simongonand@gmail.com>
2022-02-03 17:29:51 +0100

    integrate animator

--- Assets/Scripts/Controller/PlayerMovements.cs
+++ Assets/Scripts/Controller/PlayerMovements.cs
@@ -9,6 +9,7 @@ public class PlayerMovements : MonoBehaviour
     public PlayerPresets preset;
     [SerializeField]
     private Rigidbody2D rb;
+    public Animator animator;
 
     private Vector2 movementInputs;
 
@@ -17,10 +18,17 @@ public class PlayerMovements : MonoBehaviour
         movementInputs = context.ReadValue<Vector2>();
     }
 
+    private void Start()
+    {
+        animator.SetBool("Aiming", true);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 movement = rb.position + movementInputs * preset.speed * Time.fixedDeltaTime;
+        if (movement != Vector3.zero)
+            animator.SetFloat("Speed", 1);
         rb.MovePosition(movement);
 
         Vector3 cameraPos = self.position;
