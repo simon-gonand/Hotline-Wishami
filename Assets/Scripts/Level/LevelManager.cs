f143b67ced28c63acb4f6f71814059f1111c5932
Simon Gonand <simongonand@gmail.com>
2022-02-09 16:26:22 +0100

    link outro scene

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -28,6 +29,6 @@ public class LevelManager : MonoBehaviour
 
     private void EndLevel()
     {
-        Debug.Log("level finished");
+        SceneManager.LoadScene(3);
     }
 }
